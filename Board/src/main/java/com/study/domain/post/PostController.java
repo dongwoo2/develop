9504b80e5ced4d88bc17465acc70bebcf6857131
dongwoo2 <ieiedong@naver.com>
2023-06-15 22:12:09 +0900

    20230615오류못잡음

--- Board/src/main/java/com/study/domain/post/PostController.java
+++ Board/src/main/java/com/study/domain/post/PostController.java
@@ -54,15 +54,28 @@ public class PostController {
         return "redirect:/post/list.do";
     }
 
+    // 쿼리 스트링 파라미터를 Map에 담아 반환
+    private Map<String, Object> queryParamsToMap(final SearchDto queryParams) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("page", queryParams.getPage());
+        data.put("recordSize", queryParams.getRecordSize());
+        data.put("pageSize", queryParams.getPageSize());
+        data.put("keyword", queryParams.getKeyword());
+        data.put("searchType", queryParams.getSearchType());
+        return data;
+    }
+    // 게시글 삭제
     @PostMapping("/post/delete.do")
-    public String deletePost(@RequestParam final long id, Model model) {
+    public String deletePost(@RequestParam final Long id, final SearchDto queryParams, Model model) {
         postService.deletePost(id);
-        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
-        return "redirect:/post/list.do";
+        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, queryParamsToMap(queryParams));
+        return showMessageAndRedirect(message, model);
     }
 
     private String showMessageAndRedirect(final MessageDto params, Model model) {
         model.addAttribute("params", params);
         return "common/messageRedirect";
     }
+
+
 }
