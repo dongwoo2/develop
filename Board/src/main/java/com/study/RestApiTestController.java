0b84403316b4e9acce0c7eac5bfe55edc8a7e89c
dongwoo2 <ieiedong@naver.com>
2023-06-18 21:29:03 +0900

    20230618 java

--- Board/src/main/java/com/study/RestApiTestController.java
+++ Board/src/main/java/com/study/RestApiTestController.java
@@ -1,22 +1,32 @@
 package com.study;
 
-import com.study.common.dto.SearchDto;
-import com.study.common.paging.PagingResponse;
-import com.study.domain.post.PostResponse;
-import com.study.domain.post.PostService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
-@RequiredArgsConstructor
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
 public class RestApiTestController {
 
-    private final PostService postService;
+    @GetMapping("/members")
+    @ResponseBody // public @ResponseBody List<Map<String, Object>> findAllMember()와 같이 리턴 타입 앞에도 선언 가능
+    public List<Map<String, Object>> findAllMember() {
 
-    @GetMapping("/posts")
-    public PagingResponse<PostResponse> findAllPost() {
-        return postService.findAllPost(new SearchDto());
+        List<Map<String, Object>> members = new ArrayList<>();
+
+        for (int i = 1; i <= 20; i++) {
+            Map<String, Object> member = new HashMap<>();
+            member.put("id", i);
+            member.put("name", i + "번 개발자");
+            member.put("age", 10 + i);
+            members.add(member);
+        }
+
+        return members;
     }
 
 }
