190c6c2bea50b08726495488f84f4256aa7ca1a4
dongwoo2 <ieiedong@naver.com>
2023-06-19 20:42:10 +0900

    20230619 스프링부트

--- Board/src/main/java/com/study/RestApiTestController.java
+++ Board/src/main/java/com/study/RestApiTestController.java
@@ -1,32 +1,22 @@
 package com.study;
 
-import org.springframework.stereotype.Controller;
+import com.study.common.dto.SearchDto;
+import com.study.common.paging.PagingResponse;
+import com.study.domain.post.PostResponse;
+import com.study.domain.post.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class RestApiTestController {
 
-    @GetMapping("/members")
-    @ResponseBody // public @ResponseBody List<Map<String, Object>> findAllMember()와 같이 리턴 타입 앞에도 선언 가능
-    public List<Map<String, Object>> findAllMember() {
+    private final PostService postService;
 
-        List<Map<String, Object>> members = new ArrayList<>();
-
-        for (int i = 1; i <= 20; i++) {
-            Map<String, Object> member = new HashMap<>();
-            member.put("id", i);
-            member.put("name", i + "번 개발자");
-            member.put("age", 10 + i);
-            members.add(member);
-        }
-
-        return members;
+    @GetMapping("/posts")
+    public PagingResponse<PostResponse> findAllPost() {
+        return postService.findAllPost(new SearchDto());
     }
 
 }
---
+++ Board/src/main/java/com/study/domain/comment/CommentMapper.java
@@ -0,0 +1,49 @@
+package com.study.domain.comment;
+///다음은 DB와의 통신 역할을 할 Mapper 인터페이스입니다.
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+    /**
+     * 댓글 저장
+     * @param params - 댓글 정보
+     */
+    void save(CommentRequest params);
+
+    /**
+     * 댓글 상세정보 조회
+     * @param id - PK
+     * @return 댓글 상세정보
+     */
+    CommentResponse findById(Long id);
+
+    /**
+     * 댓글 수정
+     * @param params - 댓글 정보
+     */
+    void update(CommentRequest params);
+
+    /**
+     * 댓글 삭제
+     * @param id - PK
+     */
+    void deleteById(Long id);
+
+    /**
+     * 댓글 리스트 조회
+     * @param postId - 게시글 번호 (FK)
+     * @return 댓글 리스트
+     */
+    List<CommentResponse> findAll(Long postId);
+
+    /**
+     * 댓글 수 카운팅
+     * @param postId - 게시글 번호 (FK)
+     * @return 댓글 수
+     */
+    int count(Long postId);
+
+}
