8176fff1361f21a91d9f3d9359b3991d56ba5feb
dongwoo2 <ieiedong@naver.com>
2023-06-27 21:42:19 +0900

    20230627 9부터 보기

---
+++ Board/src/main/java/com/study/domain/member/MemberRequest.java
@@ -0,0 +1,31 @@
+package com.study.domain.member;
+
+import java.time.LocalDateTime;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.thymeleaf.util.StringUtils;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberRequest {
+
+    private Long id;                        // 회원 번호(PK)
+    private String loginId;                 // 로그인 ID
+    private String password;                // 비밀번호
+    private String name;                    // 이름
+    private Gender gender;                  // 성별
+    private LocalDateTime birthday;         // 생년월일
+
+    public void encodingPassword(PasswordEncoder passwordEncoder){
+        if(StringUtils.isEmpty(password)) {
+            return;
+        }
+        password = passwordEncoder.encode(password);
+    }
+
+
+}
